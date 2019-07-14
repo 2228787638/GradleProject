5af23c533f845bdbe77eb5a318ceb526dc757c89
2228787638 <2228787638@qq.com>
2019-07-14 23:50:41 +0800

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("This is a Gradle Project!");
+    }
+    public String test(){
+        return "This is a Gradle Project";
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class HelloWorldTest {
+    @Test
+    public void testAppHasAGreeting() {
+        HelloWorld helloWorld = new HelloWorld();
+        assertNotNull("app should have a greeting", helloWorld.test());
+    }
+    @Test
+    public void testMockClass() throws Exception {
+        // you can mock concrete classes, not only interfaces
+        LinkedList mockedList = mock(LinkedList.class);
+
+        // stubbing appears before the actual execution
+        String value = "first";
+        when(mockedList.get(0)).thenReturn(value);
+
+        assertEquals(mockedList.get(0), value);
+
+    }
+}
